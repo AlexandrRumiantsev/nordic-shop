c5c39c76496d1e878ccfd32e3a0cdfb325146401
alex <tte577es7@yandex.ru>
2023-01-28 12:05:22 +0300

    less-49-1

--- src/pages/detail/detail.tsx
+++ src/pages/detail/detail.tsx
@@ -1,3 +1,38 @@
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from 'react'
+
+import { iGood } from '../../components/good-list/types'
+
+ 
+import goodsJSON from '../../stub/goods.json'
+
+
 export function Detail(){
-    return <>detail</>
+
+    const { id } = useParams()
+    const [ good, setGood ] = useState({
+        TITLE: '',
+        DISCR: '',
+        PRICE: '', 
+        IMG: '',
+        COUNT: '', 
+    })
+
+    useEffect(()=>{
+        const good = goodsJSON.find((good: iGood) => 
+            good.ID === id
+        ) as iGood
+
+        setGood(good)
+    }, [])
+
+    return <>
+        <p>{id}</p>
+        <img src={good.IMG}/>
+        <p>{good.TITLE}</p>
+        <p>{good.DISCR}</p>
+        <p>{good.COUNT}</p>
+        <p>{good.PRICE}</p>
+        <button>Добавить в корзину</button>
+    </>
 }
